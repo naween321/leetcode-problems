7390cb0c56f6780dedc84ee8a7e7e156a1b59513
npaudyal321 <naween321@gmail.com>
2022-12-27 13:38:54 +0545

    two sum problem

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up
+# to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            print(hashmap)
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+
+
+obj = Solution()
+print(obj.twoSum([8, 2, 3, 5, 5], 10))
