bde6096110627500c206f878003e0ff127e2cb1d
npaudyal321 <naween321@gmail.com>
2023-01-04 23:19:07 +0545

    plus one problem solution

---
+++ plus_one.py
@@ -0,0 +1,22 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the
+# integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer
+# does not contain any leading 0's. Increment the large integer by one and return the resulting array of digits.
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        num = 0
+        for digit in digits:
+            num = num * 10 + digit
+        num += 1
+        result = []
+        while num != 0:
+            r = num % 10
+            result.append(r)
+            num = num // 10
+        result.reverse()
+        return result
+
+
+obj = Solution()
+print(obj.plusOne([9]))
