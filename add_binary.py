0c24fd5a9737fa5fb9cc4a4ad22ef936bd631a69
npaudyal321 <naween321@gmail.com>
2023-01-05 23:08:17 +0545

    add binary addition problem

---
+++ add_binary.py
@@ -0,0 +1,9 @@
+# Given two binary strings a and b, return their sum as a binary string.
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
