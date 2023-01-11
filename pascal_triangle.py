7a04a2bb66ab8cd73384ac12a8451740eba84c70
npaudyal321 <naween321@gmail.com>
2023-01-11 22:01:27 +0545

    add pascal triangle problem

---
+++ pascal_triangle.py
@@ -0,0 +1,9 @@
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+# In Pascal's triangle, each number is the sum of the two numbers directly
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
