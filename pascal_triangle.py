ccd73fb926e05a528549a15eed4afb50fe09dd20
npaudyal321 <naween321@gmail.com>
2023-01-12 23:40:15 +0545

    pascals triangle solved

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -11,5 +11,21 @@ class Solution(object):
         for i in range(numRows):
             if i == 0:
                 res.append([1])
-            if i == 1:
+            elif i == 1:
                 res.append([1, 1])
+            else:
+                last = res[-1]
+                new = []
+                for index, j in enumerate(last):
+                    if index == 0:
+                        new.append(1)
+                    else:
+                        val = j + last[index-1]
+                        new.append(val)
+                new.append(1)
+                res.append(new)
+        return res
+
+
+obj = Solution()
+print(obj.generate(1))
